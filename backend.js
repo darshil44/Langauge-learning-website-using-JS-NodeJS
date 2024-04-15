eb698ffb0329dcb6b1aa06fc3e06b303534d07c1
Darshil Dobariya <89260736+darshil44@users.noreply.github.com>
2024-04-15 09:46:38 +0530

    Update backend.js

--- backend.js
+++ backend.js
@@ -8,6 +8,8 @@ app.use(express.json());
 mongoose.connect('mongodb://localhost:27017/language_learning_app', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Error connecting to MongoDB:', err));
+
+
 const userSchema = new mongoose.Schema({
     username: { type: String, unique: true },
     password: String,
